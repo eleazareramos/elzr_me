664639f6150fa29a573a00d5d4551a5f5983c62d
Eleazar Ramos <e@fureyfs.com>
2020-04-26 14:53:21 -0400

    added link to github

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
 import Layout from '../components/Layout/Layout'
 import '../styles/global.css'
 
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  footerText: {
+    fontSize: '10px',
+    marginBottom: 5,
+    cursor: 'pointer',
+    textDecoration: 'underline',
+    color: 'grey'
+  },
+}
+
 const App = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div style={styles.root}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <span
+        style={styles.footerText}
+        onClick={() => window.open('https://github.com/eleazareramos/elzr_me')}
+      >
+        the unimpressive source code to this website.
+      </span>
+    </div>
   )
 }
 
