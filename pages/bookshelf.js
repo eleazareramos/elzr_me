fb7091aef4af23106de933f6d244e3055dadbf51
Eleazar Ramos <e@fureyfs.com>
2020-04-27 10:12:09 -0400

    fixed bookshelf text

--- pages/bookshelf.js
+++ pages/bookshelf.js
@@ -1,7 +1,7 @@
 import WorkInProgress from '../components/WorkInProgress'
 
 const BookPage = () => (
-  <WorkInProgress text="reading is tight. i'll recommend some here." />
+  <WorkInProgress text="reading is tight. i'll recommend some books here." />
 )
 
 export default BookPage
