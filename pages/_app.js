37dc6b25206212980652ddba6069909083ec9733
Eleazar Ramos <e@fureyfs.com>
2020-05-06 10:10:06 -0400

    simplified appjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import "../styles/global.css";
-import { useRouter } from "next/router";
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter();
-  if (router.pathname === "/admin") return <Component {...pageProps} />;
   return (
     <>
       <Head>
