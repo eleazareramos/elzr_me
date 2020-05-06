feee1b603b36595991f729834a41b9636479919d
Eleazar Ramos <e@fureyfs.com>
2020-05-06 10:09:18 -0400

    fixed some resume typos

--- components/About/Resume.js
+++ components/About/Resume.js
@@ -1,10 +1,10 @@
-import { colors } from '../../styles/theme'
-import FadeIn from 'react-fade-in'
-import Experience from './Experience'
-import useSmallVw from '../../hooks/useSmallVw'
+import { colors } from "../../styles/theme";
+import FadeIn from "react-fade-in";
+import Experience from "./Experience";
+import useSmallVw from "../../hooks/useSmallVw";
 
-const FUREY_HEADCOUNNT = 16
-const HEADCOUNT_AS_OF = "May '20"
+const FUREY_HEADCOUNNT = 16;
+const HEADCOUNT_AS_OF = "May '20";
 
 const styles = {
   title: {
@@ -13,10 +13,10 @@ const styles = {
     marginBottom: 20,
   },
   container: {
-    backgroundColor: 'white',
-    margin: '20px 5px',
-    padding: '20px',
-    boxShadow: '0px 3px 6px black',
+    backgroundColor: "white",
+    margin: "20px 5px",
+    padding: "20px",
+    boxShadow: "0px 3px 6px black",
   },
   basicText: {
     fontSize: 12,
@@ -24,44 +24,44 @@ const styles = {
   },
   asteriskText: {
     fontSize: 10,
-    color: 'grey',
+    color: "grey",
   },
   highlightText: {
     color: colors.highlight,
   },
   educationContainer: {
-    display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
   },
   educationHeader: {
     fontSize: 16,
     marginBottom: 5,
     color: colors.widget,
   },
-  educationText: textAlign => ({
+  educationText: (textAlign) => ({
     fontSize: 14,
-    textAlign
+    textAlign,
   }),
-}
+};
 
 const Resume = () => {
-  const smallVw = useSmallVw()
+  const smallVw = useSmallVw();
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <FadeIn>
         <div style={{ ...styles.container, width: !smallVw && 850 }}>
           <h3 style={styles.title}>CURRICULUM VITAE</h3>
           <Experience
             title="COO & Co-Founder"
-            subTitle={'Furey'}
+            subTitle={"Furey"}
             period="2017+"
           >
             <li style={styles.basicText}>
-              Led a bootstrapped company of {FUREY_HEADCOUNNT} employees{' '}
+              Led a bootstrapped company of {FUREY_HEADCOUNNT} employees{" "}
               <span style={styles.asteriskText}>
-                (headcount as of {HEADCOUNT_AS_OF}){' '}
+                (headcount as of {HEADCOUNT_AS_OF}){" "}
               </span>
               in providing white-glove accounting & finance services to high
               growth companies - disrupting internal business processes across
@@ -70,8 +70,8 @@ const Resume = () => {
               and much much more.
             </li>
             <li style={styles.basicText}>
-              Developed a Month-End-Close task management SaaS application from
-              the ground up in house{' '}
+              Internally developed a Month-End-Close task management SaaS
+              application from the ground up{" "}
               <span style={styles.asteriskText}>
                 (deployed internally on web, public beta launch in progress,
                 mobile in progress)
@@ -84,20 +84,20 @@ const Resume = () => {
               Created & managed the "Innovations" team, whose main function is
               to develop tools that make our clients' lives easier. Some
               examples: a Google Sheet Add-On to push & pull data to & from
-              Quickbooks Online, a Python notebook to fully reconciles Shopify
-              Data, Shipping Data and Payment Data (3 way match), a dynamic
-              dashboard to view real-time Budget v. Actual financials.
+              Quickbooks Online, a Python notebook to fully reconcile Shopify,
+              shipping, and payment data (3 way match), and a dynamic dashboard to
+              view real-time Budget v. Actual financials.
             </li>
             <li style={styles.basicText}>
-              Played a key role in our clients' fundraising efforts{' '}
-              <span style={styles.asteriskText}>(from Seed to Series D)</span>{' '}
+              Played a key role in our clients' fundraising efforts{" "}
+              <span style={styles.asteriskText}>(from Seed to Series D)</span>{" "}
               by creating pro forma models, fielding financial due dilligence
               questions, and providing statement commentary.
             </li>
           </Experience>
           <Experience
             title="Finance Associate"
-            subTitle={'Founder Shield'}
+            subTitle={"Founder Shield"}
             period="2017"
           >
             <li style={styles.basicText}>
@@ -119,17 +119,17 @@ const Resume = () => {
           </Experience>
           <Experience
             title="Project Specialist < Accounting Analyst < Intern"
-            subTitle={'HelloFresh'}
+            subTitle={"HelloFresh"}
             period="2015-17"
           >
             <li style={styles.basicText}>
               Independently identified the need for a purchase order to bill
-              reconciliation procedure{' '}
+              reconciliation procedure{" "}
               <span style={styles.asteriskText}>
                 (to reconcile 100+ bills per day)
               </span>
               . Architected an Excel solution (w/ Macros & Power BI), which
-              consolidates data and summarizes discrepancies{' '}
+              consolidates data and summarizes discrepancies{" "}
               <span style={styles.asteriskText}>
                 (still being used to this day per an old colleague!)
               </span>
@@ -141,13 +141,13 @@ const Resume = () => {
           </Experience>
           <Experience title="The Starter Jobs" period="2012-15">
             <li style={styles.basicText}>
-              Clerical documentation for mortgage processing.{' '}
+              Clerical documentation for mortgage processing.{" "}
               <span style={styles.highlightText}>
                 (American Financial Resources)
               </span>
             </li>
             <li style={styles.basicText}>
-              Food & Bev: from barista to cashier to cook.{' '}
+              Food & Bev: from barista to cashier to cook.{" "}
               <span style={styles.highlightText}>
                 (Dunkin', Smashburger, A local brunch spot)
               </span>
@@ -158,18 +158,18 @@ const Resume = () => {
             <p style={styles.educationText()}>
               Fullstack Academy of Code - Coding Bootcamp
             </p>
-            <p style={styles.educationText('right')}>2018</p>
+            <p style={styles.educationText("right")}>Cohort: 2018</p>
           </div>
           <div style={styles.educationContainer}>
             <p style={styles.educationText()}>
               William Paterson University - BS, Accounting
             </p>
-            <p style={styles.educationText('right')}>2012-16</p>
+            <p style={styles.educationText("right")}>Class: 2016</p>
           </div>
         </div>
       </FadeIn>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
